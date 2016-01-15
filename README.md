280308dbba481b6aa8f314cc6b548ed32a74a620
BuildTools <unconfigured@null.spigotmc.org>
2016-01-14 17:47:23 -0800

    Rewrite, known bug in third layer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # procedural-generation-c2
 For the monthly procedural generation challange on /r/proceduralgeneration (#2 - castle)
+
+An example image can be seen in output.jpg
+
+#Features:
+Strong, central keep
+#### Castle town
+Curtain wall
+Flags, Banners
+#### Source code link
+Forces castle can support
+Utility Buildings
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var fs = require('fs')
 
 
 var image = gm('./resources/blueprint.jpg').noProfile().resize(1000,1000,"!").fontSize(10);
-var rooms = [
-]
-var features = {
+//var rooms = [
+//]
+/*var features = {
   "center":[
     {"name": "house","ammount":[20,30],"size":[[10,30],[10,30]]},
     {"name": "garden","ammount":[5,10],"size":[[10,30],[10,30]]}
@@ -13,10 +13,116 @@ var features = {
   "wall":[
     {"name":"tower","expand":10}
   ]
+}*/
+var features = [ // list of building groups
+  {
+    "name": "Castle",
+    "size": new Size(300,700,300,700),
+    "ammount": new Ammount(1,2),
+    "subgroups": [
+      {
+        "name": "Village",
+        "size": new Size(50,100,50,100),
+        "ammount": new Ammount(1,5),
+        "subgroups": [
+          {
+            "name": "House", // third subgroup layer has graphical trouble caused by Y2 position being NAN
+            "size": new Size(10,20),
+            "ammount": new Ammount(10,20),
+            "subgroups": []
+          }
+        ]
+      }
+    ]
+  }
+]
+var rooms = [ // TODO this should probably end up being a class with a .draw()
+  //{"name": "Castle", "position": {"x":2,"y":2,"x2":10,"y2":50}, "subgroups": []} // subgroups position is NOT relative to main group position
+]
+function log(){
+  // TODO uncomment the line below to enable logging to console (too lazy to add --verbose)
+  //console.log.apply(undefined,arguments)
+}
+function generateSubgroup(bg,arr,w,h){ // Generate a group of building, for example the main castle or villages
+  log("Generating Subgroup ", bg);
+  bg.forEach(function(building){
+    var ammount = building.ammount.get();
+    log("Parsing Building ", building);
+    log(ammount + " of " + building.name + " will be created");
+    for(var i = 0;i < ammount;i++){
+      var endall = false;
+      log("Creating building "+ i);
+      var size = building.size.get();
+      var pos = randPos(size,w,h);
+      var wi = 0;
+      while(testCollision(arr,pos.x,pos.y,pos.x2,pos.y2) != 0){
+        log("Random position was not valid. Trying again (" + wi +"/19 times)");
+        wi++;
+        if(wi > 20){
+          console.log("Could not solve position, you may be missing a building");
+          endall = true;
+          break;
+        }
+        pos = randPos(size,w,h);
+      }
+      if(!endall)
+      {generateSubgroup(
+        building.subgroups,
+        registerRoom(arr,building.name,pos.x,pos.y,pos.x2,pos.y2),
+        pos.x2-pos.x,
+        pos.y2-pos.y
+      );}
+    }
+  });
+}
+
+function randPos(size,w,h){
+  log("Creating building of ",size, " with constraints " + w + " " + h);
+  var ans ={"x": randi(0,w-size.w),"y": randi(0,h-size.h)};
+  ans.x2 = ans.x + size.w;
+  ans.y2 = ans.y + size.h;
+  log("Random position for building is ", ans);
+  return(ans);
+}
+
+function registerRoom(arr,name,x,y,x2,y2,drawCallback,nameCallback){
+  var sg = [];
+  var toPush ={"name": name, "position": {"x": x, "y": y, "x2": x2,"y2":y2},"subgroups":sg};
+  if(drawCallback) toPush.draw = drawCallback;
+  if(nameCallback) toPush.name = nameCallback();
+  arr.push(toPush);
+  return sg;
+}
+
+function testCollision(arr,x,y,x2,y2){
+  var count = 0;
+  arr.forEach(function(pos){
+    var cx = pos.position.x;
+    var cy = pos.position.x;
+    var cx2 = pos.position.x2;
+    var cy2 = pos.position.y2;
+    if(x >= cx && y >= cy && x2 <= cx2 && y2 <= cy2){
+      count += 1;
+    }
+  });
+  return count;
+}
+
+function Size(w,w2,h,h2){
+  this.w = w;this.w2 = w2;this.h = h;this.h2 = h2;
+}
+Size.prototype.get = function(){
+  return {"w": randi(this.w,this.w2), "h": randi(this.h,this.h2)};
+};
+function Ammount(a,b){
+  this.ammount = [a,b]
+}
+Ammount.prototype.get = function(){
+  return randi(this.ammount[0],this.ammount[1])
 }
 
 function drawRoom(name,x,y,w,h){
-  console.log(name,x,y,w,h);
+  log(name,x,y,w,h);
   image.stroke("#ffffff").fill('none')
     .drawRectangle(x,y,x+w,y+h)
   //  .drawText(((w/2)+x)-500,((h/2)+y)-500,name,'Center');
@@ -36,31 +142,24 @@ function addRoom(name,x1,y1,x2,y2){
   rooms.push({"name":name,"x":x1,"y":y1,"x2":x2,"y2":y2})
 }
 function randi(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
-function createCastle(){
-  // main room
-  var center = {
-    x:randi(200,500),y:randi(200,500)
-  }
-  center.x2 = center.x+randi(200,500);center.y2 = center.y+randi(200,500)
-  addRoom("",center.x,center.y,center.x2,center.y2);
-  features.center.forEach(function(feature){
-    var ammount = randi.apply(undefined,feature.ammount);
-    for(var i = 0;i <= ammount;i++){
-      var featuresize = [randi(feature.size[0][0],feature.size[0][1]),randi(feature.size[1][0],feature.size[1][1])]
-      var featurepos = {x:randi(center.x,center.x2-featuresize[0]),y:randi(center.y,center.y2-featuresize[1])};
-      featurepos.x2 = featurepos.x + featuresize[0];featurepos.y2 = featurepos.y + featuresize[1];
-      addRoom(feature.name,featurepos.x,featurepos.y,featurepos.x2,featurepos.y2)
-    }
+function drawCastle(data,origx,origy){
+  data.forEach(function(room){
+    var roomDraw = drawRoom;
+    if(room.drawCallback) roomDraw = drawCallback;
+    var x = room.position.x + origx;
+    var y = room.position.y + origy;
+    var w = room.position.x2 - room.position.x; // can't just use x/y because they include the orig changes
+    var h = room.position.y2 - room.position.y;
+    roomDraw(room.name,x,y,w,h);
+    drawCastle(room.subgroups,x,y);
   });
-  var size = {
-    wh: randi(25,50)
-  }
-  addRoom("",center.x - size.wh,center.y - size.wh, center.x2 + size.wh, center.y2 + size.wh);
 }
-createCastle();
-rooms.forEach(function(room){
-  drawRoom(room.name,room.x,room.y,room.x2 - room.x,room.y2 - room.y);
-});
+generateSubgroup(features,rooms,1000,1000);
+
+
+log(JSON.stringify(rooms));
+drawCastle(rooms,0,0);
 save();
+//log(randi(0,50));
